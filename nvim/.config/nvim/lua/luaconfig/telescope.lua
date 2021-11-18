2f7da8b2197b28922d69f0d9e08cb4987018176e
Ismael Padilla <padillaismael92@gmail.com>
2021-11-17 23:56:03 -0300

    add telescope config

---
+++ nvim/.config/nvim/lua/luaconfig/telescope.lua
@@ -0,0 +1,12 @@
+require('telescope').setup {
+    defaults = {
+        --prompt_prefix = ' >'
+    },
+    extensions = {
+        fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+        }
+    }
+}
+require('telescope').load_extension('fzy_native')
