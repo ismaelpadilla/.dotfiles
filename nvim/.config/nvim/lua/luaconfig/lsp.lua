168058184f472260427662e71f6bce4206a69cf7
Ismael Padilla <padillaismael92@gmail.com>
2022-03-20 18:04:31 -0300

    Clean up nvim lsp config a little

--- nvim/.config/nvim/lua/luaconfig/lsp.lua
+++ nvim/.config/nvim/lua/luaconfig/lsp.lua
@@ -5,10 +5,7 @@ cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
             require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-            -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
         end,
     },
     mapping = {
@@ -24,10 +21,7 @@ cmp.setup({
     },
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'vsnip' }, -- For vsnip users.
-        -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
+        { name = 'luasnip' }, -- For luasnip users.
     }, {
         { name = 'buffer' },
     })
@@ -55,12 +49,6 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 --require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
 --capabilities = capabilities
 --}
-
-
---require("lspconfig").tsserver.setup{ capabilities = capabilities }
---require("lspconfig").rust_analyzer.setup{ capabilities = capabilities }
---require("lspconfig").sumneko_lua.setup{ capabilities = capabilities }
---require("lspconfig").tsserver.setup(capabilities = capabilities)
 
 local nvim_lsp = require('lspconfig')
 
