98c89b4db207a7dd48181ee6d6f447c06516a2aa
Ismael Padilla <padillaismael92@gmail.com>
2022-03-20 16:32:19 -0300

    Add angular lsp config to nvim

--- nvim/.config/nvim/lua/luaconfig/lsp.lua
+++ nvim/.config/nvim/lua/luaconfig/lsp.lua
@@ -189,3 +189,11 @@ require("lspconfig").gopls.setup({
 		},
 	},
 })
+
+require'lspconfig'.angularls.setup{
+        on_attach = on_attach,
+        flags = {
+            debounce_text_changes = 150,
+        },
+        capabilities = capabilities
+}
