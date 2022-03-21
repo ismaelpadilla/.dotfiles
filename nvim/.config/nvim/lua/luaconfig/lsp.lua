b41593d59388f0097667722b274cc1854bb501b1
Ismael Padilla <padillaismael92@gmail.com>
2022-03-20 21:03:11 -0300

    cmp config

--- nvim/.config/nvim/lua/luaconfig/lsp.lua
+++ nvim/.config/nvim/lua/luaconfig/lsp.lua
@@ -1,3 +1,5 @@
+local lspkind = require('lspkind')
+
 -- Setup nvim-cmp. https://github.com/hrsh7th/nvim-cmp
 local cmp = require'cmp'
 
@@ -12,19 +14,48 @@ cmp.setup({
         ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
         ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+        -- ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
         ['<C-e>'] = cmp.mapping({
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
         }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
+        { name = 'path' },
+        { name = 'nvim_lua' },
         { name = 'luasnip' }, -- For luasnip users.
-    }, {
         { name = 'buffer' },
-    })
+    }),
+
+    formatting = {
+        format = lspkind.cmp_format({
+          with_text = true,
+          -- mode = 'symbol', -- show only symbol annotations
+          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+          -- The function below will be called before any actual modifications from lspkind
+          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          -- before = function (entry, vim_item)
+          --   return vim_item
+          -- end
+
+          menu = {
+              buffer = "[buf]",
+              nvim_lsp = "[LSP]",
+              nvim_lua = "[api]",
+              path = "[path]",
+              luasnip = "[snip]",
+          }
+        })
+    },
+    experimental = {
+        ghost_text = true
+    },
+    view = {
+        entries = "native"
+    }
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
