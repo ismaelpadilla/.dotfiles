6085e14bb86f56df4e6f42eb9ccb5fbf9f20a6d1
ismaelpadilla <padillaismael92@gmail.com>
2022-07-01 13:37:51 -0300

    nvim jdtls conf

---
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -0,0 +1,118 @@
+P('init jdtls')
+local home = os.getenv('HOME')
+local root_markers = { 'gradlew', 'pom.xml' }
+local root_dir = require('jdtls.setup').find_root(root_markers)
+
+local project_name = vim.fn.fnamemodify(root_dir, ":p:h:t") -- https://github.com/mfussenegger/nvim-jdtls/wiki/Sample-Configurations
+-- local project_name = '/.workspace' .. vim.fn.fnamemodify(root_dir, ":p:h:t") -- https://github.com/mfussenegger/nvim-jdtls/wiki/Sample-Configurations
+
+-- local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+
+local workspace_dir = home .. '/.jdt-workspace/' .. project_name
+-- local workspace_dir = '~/RepoGit/' .. 'analytics-services'
+
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+local config = {
+    -- The command that starts the language server
+    -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+    cmd = {
+
+        -- 💀
+        '/usr/lib/jvm/java-11-openjdk-amd64/bin/java', -- or '/path/to/java11_or_newer/bin/java'
+        -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+        '-Dosgi.bundles.defaultStartLevel=4',
+        '-Declipse.product=org.eclipse.jdt.ls.core.product',
+        '-Dlog.protocol=true',
+        '-Dlog.level=ALL',
+        '-Xms1g',
+        '--add-modules=ALL-SYSTEM',
+        '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+        '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+        -- 💀
+        -- '-jar', '/path/to/jdtls_install_location/plugins/org.eclipse.equinox.launcher_VERSION_NUMBER.jar',
+        '-jar', home .. '/jdt/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
+        -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+        -- Must point to the                                                     Change this to
+        -- eclipse.jdt.ls installation                                           the actual version
+
+
+        -- 💀
+        -- '-configuration', '/path/to/jdtls_install_location/config_SYSTEM',
+        '-configuration', home .. '/jdt/config_linux',
+        -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+        -- Must point to the                      Change to one of `linux`, `win` or `mac`
+        -- eclipse.jdt.ls installation            Depending on your system.
+
+
+        -- 💀
+        -- See `data directory configuration` section in the README
+        '-data', workspace_dir
+    },
+
+    -- 💀
+    -- This is the default if not provided, you can remove it. Or adjust as needed.
+    -- One dedicated LSP server & client will be started per unique root_dir
+    -- root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew' }),
+
+    -- Here you can configure eclipse.jdt.ls specific settings
+    -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+    -- for a list of options
+    settings = {
+        java = {
+            signatureHelp = { enabled = true };
+        }
+    },
+
+    -- Language server `initializationOptions`
+    -- You need to extend the `bundles` with paths to jar files
+    -- if you want to use additional eclipse.jdt.ls plugins.
+    --
+    -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+    --
+    -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+    init_options = {
+        bundles = {}
+    },
+}
+-- This starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
+require('jdtls').start_or_attach(config)
+
+
+local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(0, ...) end
+
+local function buf_set_option(...) vim.api.nvim_buf_set_option(0, ...) end
+
+-- Enable completion triggered by <c-x><c-o>
+buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+-- Mappings.
+local opts = { noremap = true, silent = true }
+
+-- See `:help vim.lsp.*` for documentation on any of the below functions
+-- regurar nvim mappings
+buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+buf_set_keymap('n', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+buf_set_keymap('n', '<space>CA', '<cmd>:CodeActionMenu<CR>', opts)
+buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+buf_set_keymap('n', '<space>ld', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+buf_set_keymap('n', '<space>ql', '<cmd>lua vim.lsp.diagnostic.set_qflist()<CR>', opts)
+buf_set_keymap('n', '<space>ll', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+buf_set_keymap('n', '<space>fc', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+-- jdtls-specific
+buf_set_keymap('v', '<space>lem', '<Esc><Cmd>lua require("jdtls").extract_method(true)<CR>', opts)
