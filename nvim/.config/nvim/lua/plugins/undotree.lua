dde764fd9d4fd2c0856410e3d8a428e0f1539d3c
Ismael Padilla <padillaismael92@gmail.com>
2024-02-28 23:28:08 -0300

    Enable persistent undo

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -2,5 +2,7 @@ return {
     "mbbill/undotree",
     config = function()
         vim.api.nvim_set_keymap('n', '<leader>u', '<cmd>UndotreeToggle<CR>', {})
+
+        vim.o.undofile = true
     end
 }
